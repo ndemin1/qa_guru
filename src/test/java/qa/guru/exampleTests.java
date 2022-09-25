b557342173cc06f1e36a066ae3af6caa485739c4
Никита Демин <ndemin@sberhealth.ru>
2022-09-25 14:47:22 +0300

    create project

---
+++ src/test/java/qa/guru/exampleTests.java
@@ -0,0 +1,14 @@
+package qa.guru;
+
+import org.junit.jupiter.api.Assertions;
+
+public class exampleTests {
+
+    public void exampleTests1() {
+        Assertions.assertEquals(1, 2);
+    }
+
+    public void exampleTest2() {
+        Assertions.assertEquals(13, 13);
+    }
+}
